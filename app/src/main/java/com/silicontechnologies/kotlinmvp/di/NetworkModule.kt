7dab85242a224771c641fa607f350e2b99c6d824
Raja Jawahar <rajajawahar77@gmail.com>
2018-03-21 18:31:36 +0530

    Jackson convertor factory added in network module

--- app/src/main/java/com/silicontechnologies/kotlinmvp/di/NetworkModule.kt
+++ app/src/main/java/com/silicontechnologies/kotlinmvp/di/NetworkModule.kt
@@ -1,6 +1,14 @@
 package com.silicontechnologies.kotlinmvp.di
 
+import com.silicontechnologies.kotlinmvp.PostApi
+import com.silicontechnologies.kotlinmvp.base.AppConstatns.Companion.BASE_URL
 import dagger.Module
+import dagger.Provides
+import dagger.Reusable
+import io.reactivex.schedulers.Schedulers
+import retrofit2.Retrofit
+import retrofit2.adapter.rxjava2.RxJava2CallAdapterFactory
+import retrofit2.converter.jackson.JacksonConverterFactory
 
 /**
  * Created by rajajawahar on 20/03/18.
@@ -8,6 +16,27 @@ import dagger.Module
 @Module
 object NetworkModule {
 
+    @Provides
+    @Reusable
+    @JvmStatic
+    internal fun providePostApi(retrofit: Retrofit): PostApi {
+        return retrofit.create(PostApi::class.java)
+    }
 
+    /**
+     * Provides the Retrofit object.
+     * @return the Retrofit object
+     */
+    @Provides
+    @Reusable
+    @JvmStatic
+    internal fun provideRetrofitInterface(): Retrofit {
+        return Retrofit.Builder()
+                .baseUrl(BASE_URL)
+                .addConverterFactory(JacksonConverterFactory.create())
+                .addCallAdapterFactory(
+                        RxJava2CallAdapterFactory.createWithScheduler(Schedulers.io()))
+                .build()
+    }
 
 }
